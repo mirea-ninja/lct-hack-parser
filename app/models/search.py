7162758884771cda6285975b188b071344c3e081
Dragonprod <artyom2002-98@mail.ru>
2022-11-02 22:04:39 +0300

    fix: add query guid and subquery guid in Search model

--- app/models/search.py
+++ app/models/search.py
@@ -1,9 +1,13 @@
+from uuid import UUID
+
 from pydantic import BaseModel
 
 from app.models.enums import Segment, Walls
 
 
 class SearchBase(BaseModel):
+    query_id: UUID
+    subquery_id: UUID
     address: str
     rooms: int
     segment: Segment
